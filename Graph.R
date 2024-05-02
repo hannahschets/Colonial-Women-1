7147d6f825d0bc3bd3670a510babbc534b35f2ba
hannahschetselaar <hnschetselaar@vt.edu>
2024-05-02 13:20:02 -0400

    Make graph

--- Graph.R
+++ Graph.R
@@ -3,3 +3,21 @@
 
 
 pop = read.csv("Populations.csv")
+View(pop)
+
+library(tidyverse)
+library(ggplot2)
+
+str(pop$Date)
+#Recognizing it as integer...I need to change to a character
+year <- pop %>% mutate(Date = as.character(pop$Date))
+str(year$Date)
+#A character now
+
+graph = ggplot(data = pop, aes(x=Date, y=Population)) + 
+  geom_point() + geom_line()
+graph
+
+graph = ggplot(data = year, aes(x=Date, y=Population)) + 
+  geom_point() + geom_line()
+graph
